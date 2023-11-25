ffca1c050bd48364e6d10be84f988113aaa94745
jesusmonges <galeanojesus182@gmail.com>
2023-11-24 22:41:39 -0300

    Taller8

---
+++ php/estudiante/store.php
@@ -0,0 +1,12 @@
+<?php 
+    require_once('estudianteModel.php');
+    $object = new estudianteModel();
+
+    $nombre= $_REQUEST['nombre'];
+    $apellido= $_REQUEST['apellido'];
+    $codigocurso= $_REQUEST['codigocurso'];
+    
+    $registro = $object->insertar($nombre, $apellido,$codigocurso);
+
+    header('location: index.php')
+?>
